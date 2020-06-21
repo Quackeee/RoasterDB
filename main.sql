65cee7bb40bd5a7537cb86ef5f6e080bc6e183a5
Quackeee <krzysztofklak7@gmail.com>
2020-06-21 21:16:29 +0200

    ignoring main

--- main.sql
+++
@@ -1,9 +0,0 @@
-drop database if exists roaster;
-create database roaster;
-use roaster;
-
-source "[folder z repo]\tables.sql";
-source "[folder z repo]\views.sql";
-source "[folder z repo]\funcs.sql";
-source "[folder z repo]\users.sql";
-source "[folder z repo]\data.sql";
