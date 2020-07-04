0360a0ed8d8deda2c68e92e34d8669a803a0d335
Quackeee <krzysztofklak7@gmail.com>
2020-07-04 14:55:14 +0200

    added two initial users and roasteries

---
+++ data.sql
@@ -0,0 +1,2 @@
+insert into palarnia values ('roastery1', 'roaster1@localhost', 10000);
+insert into palarnia values ('roastery2', 'roaster2@localhost', 15000);
---
+++ users.sql
@@ -0,0 +1,5 @@
+drop user if exists roaster1@localhost
+drop user if exists roaster2@localhost
+
+create user roaster1@localhost identified by 'Zaq12wsx';
+create user roaster2@localhost identified by 'Zaq12wsx';
