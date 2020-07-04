acb9457e7f6dab5478200d0a42fa745e33fc7829
Quackeee <krzysztofklak7@gmail.com>
2020-07-04 16:20:04 +0200

    user roles and privileges

--- users.sql
+++ users.sql
@@ -1,5 +1,20 @@
-drop user if exists roaster1@localhost
-drop user if exists roaster2@localhost
+-- dodawanie ról
+drop role if exists wlasciciel_palarni;
+
+create role wlasciciel_palarni;
+grant select on roaster.moje_ziarna to wlasciciel_palarni;
+grant select on roaster.moja_palarnia to wlasciciel_palarni;
+grant select on roaster.moje_paczki to wlasciciel_palarni;
+
+-- dodawanie użytkowników
+drop user if exists roaster1@localhost;
+drop user if exists roaster2@localhost;
 
 create user roaster1@localhost identified by 'Zaq12wsx';
 create user roaster2@localhost identified by 'Zaq12wsx';
+
+grant wlasciciel_palarni to roaster1@localhost;
+grant wlasciciel_palarni to roaster2@localhost;
+
+set default role wlasciciel_palarni to roaster1@localhost;
+set default role wlasciciel_palarni to roaster2@localhost;
