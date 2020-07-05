c6916d37c1949c881dd78bb63874ca2c56d75a56
Quackeee <krzysztofklak7@gmail.com>
2020-07-05 16:56:34 +0200

    Thanks to Natalia the bug is fixed

--- funcs.sql
+++ funcs.sql
@@ -4,7 +4,7 @@ delimiter //
 create procedure sprzedaj(ktore int, ile int)
 begin
 update moje_paczki set liczba = cast(liczba as signed) - ile where ID = ktore;
-update moja_palarnia set budzet = budzet + ile * (select sum(temp.cena) from (select cena from moje_paczki where ID = id) temp);
+update moja_palarnia set budzet = budzet + ile * (select sum(temp.cena) from (select cena from moje_paczki where ID = ktore) temp);
 end//
 
 delimiter ;
