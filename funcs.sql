ae53d45546777e11352e3718f8d54c24bfd023cd
Quackeee <krzysztofklak7@gmail.com>
2020-07-05 16:54:59 +0200

    Thanks to Natalia the bug is fixed

--- funcs.sql
+++ funcs.sql
@@ -1,9 +1,9 @@
 set global log_bin_trust_function_creators = 1;
 
 delimiter //
-create procedure sprzedaj(id int, ile int)
+create procedure sprzedaj(ktore int, ile int)
 begin
-update moje_paczki set liczba = cast((cast(liczba as signed) - ile) as unsigned) where ID = id;
+update moje_paczki set liczba = cast(liczba as signed) - ile where ID = ktore;
 update moja_palarnia set budzet = budzet + ile * (select sum(temp.cena) from (select cena from moje_paczki where ID = id) temp);
 end//
 
