0a61bfd10f5a7997cc348cb7d4faabfbbf7b73a0
nszarek284 <56651868+nszarek284@users.noreply.github.com>
2020-07-07 09:29:06 +0200

    add main

---
+++ main.sql
@@ -0,0 +1,9 @@
+drop database if exists roaster;
+create database roaster;
+use roaster;
+
+source "D:\bazy_danych\RoasterDB-master\tables.sql";
+source "D:\bazy_danych\RoasterDB-master\views.sql";
+source "D:\bazy_danych\RoasterDB-master\funcs.sql";
+source "D:\bazy_danych\RoasterDB-master\users.sql";
+source "D:\bazy_danych\RoasterDB-master\data.sql";
